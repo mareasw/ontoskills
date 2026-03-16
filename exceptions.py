2d64dd41aae3d870bb0631810f5821bdf5fb4594
Marcello <marcellorusso.x@gmail.com>
2026-03-16 01:09:21 +0100

    feat: define custom exception classes
    Add SkillETLError base class and specific exceptions:
    - OntologyLoadError (exit_code=5)
    - SecurityError (exit_code=3)
    - ExtractionError (exit_code=4)
    - SPARQLError (exit_code=6)
    - SkillNotFoundError (exit_code=7)

---
+++ exceptions.py
@@ -0,0 +1,22 @@
+class SkillETLError(Exception):
+    exit_code: int = 1
+
+
+class OntologyLoadError(SkillETLError):
+    exit_code = 5
+
+
+class SecurityError(SkillETLError):
+    exit_code = 3
+
+
+class ExtractionError(SkillETLError):
+    exit_code = 4
+
+
+class SPARQLError(SkillETLError):
+    exit_code = 6
+
+
+class SkillNotFoundError(SkillETLError):
+    exit_code = 7
---
+++ tests/test_exceptions.py
@@ -0,0 +1,7 @@
+import pytest
+from exceptions import SkillETLError, SecurityError
+
+
+def test_exceptions_exit_codes():
+    assert SkillETLError.exit_code == 1
+    assert SecurityError.exit_code == 3
